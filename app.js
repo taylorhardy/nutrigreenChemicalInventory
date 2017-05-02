48c3af17830155316a5b27e1c0303b31b0eaf98a
taylorhardy <446341@directv.com>
2017-05-02 17:16:29 -0500

    fixed comment

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
-Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+//Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
 // Connect to the database before starting the application server.
