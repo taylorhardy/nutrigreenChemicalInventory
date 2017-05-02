c29fe29e72f609118dad7296c8b8c1566e3e0175
taylorhardy <446341@directv.com>
2017-05-02 16:32:34 -0500

    trying again

--- app.js
+++ app.js
@@ -10,7 +10,32 @@ var users = require('./routes/users');
 
 var app = express();
 
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+	if (err) {
+		console.log(err);
+		process.exit(1);
+	}
+
+	// Save database object from the callback for reuse.
+	db = database;
+	console.log("Database connection ready");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
