f853cc8f31ef2fa1920fdf688e69a5f7a03e6a23
taylorhardy <446341@directv.com>
2017-05-03 12:07:32 -0500

    checking db connection info

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(distDir));
 
 //Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
-console.log(process.env.MONGODB_URI);
+console.log("mongo uri",process.env.MONGODB_URI);
 // Connect to the database before starting the application server.
 // mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
 // 	if (err) {
