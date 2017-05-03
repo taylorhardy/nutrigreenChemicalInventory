8ad45321a82d787324adcc10e7f345fb558beb45
taylorhardy <446341@directv.com>
2017-05-03 16:54:28 -0500

    recommiting

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ if (app.get('env') === 'development') {
 		res.status(err.status || 500);
 		res.render('error', {
 			message: err.message,
-			error: err,
+			error: err
 		});
 	});
 }
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error', {
 		message: err.message,
-		error: {},
+		error: {}
 	});
 });
 
